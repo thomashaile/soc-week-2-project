318a7db34ccc0c3cbb60a9108ee2e88692c58b55
Thomas Haile <59530856+thomashaile@users.noreply.github.com>
2020-02-14 23:45:20 +0100

    Update repeater.js
    repeater function added

--- challenges/repeater.js
+++ challenges/repeater.js
@@ -1,7 +1,26 @@
 console.log('repeater script has loaded');
 
 // write this function!
-function repeater() {
+function repeater(userPhrase, userNum) {
+  
+  //using if else statment
+  /*if (userNumStr < 0) return "";
+  else if (userNum === 1) return userPhrase;
+  else return (userPhrase + "\n").repeat(userNum);
+*/
+
+  //using switch case
+  switch (true) {
+    case userNum < 0:
+      return "";
+      break;
+    case userNum === 1:
+      return userPhrase;
+      break;
+
+    default:
+      return (userPhrase + "\n").repeat(userNum);
+  }
 }
 
 console.assert(repeater('horse', 2) === 'horse\nhorse\n', 'first');
@@ -16,7 +35,7 @@ function repeaterHandler() {
   const userPhrase = prompt('enter a phrase');
   const userNumStr = prompt('enter a num');
   // cast userNumStr to a Number, and assign the value to userNum
-
+userNum = Number(userNumStr);
   console.assert(typeof userNum === 'number', "don't forget to cast userNumStr to a number!");
 
   // perform core logic
